94e4745a3c30e80b3dbf842c25c758c8a4d4e4f8
Debora Barbosa Aires <debora.aires@unesp.br>
2026-04-01 21:25:05 -0300

    personalização da aparencia

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,23 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { LogBox } from "react-native";
+
+LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return(
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{headerShown: false}} />
-      <Stack.Screen name="+not-found" />
+    <>
+    <StatusBar style="light"/>
+    <Stack
+    screenOptions={{
+    headerStyle:{ backgroundColor:'#25292e',},
+                headerShadowVisible: false,
+                headerTintColor:'#fff',
+    }}
+    >
+      <Stack.Screen name="(tabs)" options={{headerShown: false,}} />
+      <Stack.Screen name="+not-found" options={{}}/>
     </Stack>
+    </>
   );
 }
